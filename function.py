ba28ed4467276b0f9a81f9ba6dee546cf97456b5
ilaria-giunti <102374311+ilaria-giunti@users.noreply.github.com>
2023-04-08 15:56:33 +0200

    Create function.py

---
+++ function.py
@@ -0,0 +1,48 @@
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+import PyPDF2
+import io
+import os
+import streamlit as st
+
+st.title(""" Find any information you're looking for in your PDF""")
+os.environ["OPENAI_API_KEY"] = "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"
+
+domanda=st.text_input("Ask me anything")
+uploaded_file = st.file_uploader("Upload your file", type=["pdf"])
+button = st.button("Run")
+
+if button:
+    if uploaded_file is not None and uploaded_file.name.endswith('.pdf'):
+        pdf_file = io.BytesIO(uploaded_file.getvalue())
+        reader = PyPDF2.PdfReader(pdf_file)
+
+        raw_text = ''
+        for i, page in enumerate(reader.pages):
+            text = page.extract_text()
+            if text:
+                raw_text += text
+
+        text_splitter = CharacterTextSplitter(        
+            separator = "\n",
+            chunk_size = 1000,
+            chunk_overlap  = 200,
+            length_function = len,
+        )
+        texts = text_splitter.split_text(raw_text)
+
+        embeddings = OpenAIEmbeddings()
+
+        docsearch = FAISS.from_texts(texts, embeddings)
+
+        chain = load_qa_chain(OpenAI(), chain_type="stuff")
+
+        query = domanda
+        docs = docsearch.similarity_search(query)
+        risposta=chain.run(input_documents=docs, question=query,verbose=True)
+
+        st.write(risposta)
